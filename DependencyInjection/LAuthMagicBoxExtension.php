0c7002ba36be23eb980c32d2869f5d463b96a0de
Nick Mullen <nick-mullen@hotmail.co.uk>
2015-09-01 11:36:05 +0100

    Update LAuthMagicBoxExtension.php
    added stacker config

--- DependencyInjection/LAuthMagicBoxExtension.php
+++ DependencyInjection/LAuthMagicBoxExtension.php
@@ -21,8 +21,10 @@ class LAuthMagicBoxExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+    
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('stacker.yml');
     }
 }
